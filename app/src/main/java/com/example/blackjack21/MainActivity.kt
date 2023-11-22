6996bb0d203583274227b90ad4d3df728867486d
KT <thun.kristian@gmail.com>
2023-11-22 10:52:41 +0100

    before Starting on click

--- app/src/main/java/com/example/blackjack21/MainActivity.kt
+++ app/src/main/java/com/example/blackjack21/MainActivity.kt
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fragmentContainer: FrameLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fragmentContainer = findViewById(R.id.fragmentContiner)
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
 
+        val fragment: Fragment = MainMenuFragment()
+
+        fragmentTransaction.replace(R.id.fragmentContiner, fragment)
+        fragmentTransaction.commit()
 
     }
 
