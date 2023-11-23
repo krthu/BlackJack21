4c1c998e065b407c76a87378dc0bf176a5a7125c
KT <thun.kristian@gmail.com>
2023-11-23 12:33:27 +0100

    Basic hit function in place

--- app/src/main/java/com/example/blackjack21/GameplayFragment.kt
+++ app/src/main/java/com/example/blackjack21/GameplayFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +23,18 @@ class GameplayFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var hitButton: Button
+    lateinit var playerCard1: ImageView
+    lateinit var playerCard2: ImageView
+    lateinit var playerCard3: ImageView
+    lateinit var playerCard4: ImageView
+    lateinit var playerCard5: ImageView
+    lateinit var playerCardValueTextView: TextView
+    private val playerCardImages = mutableListOf<ImageView>()
+    val game = Game()
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +49,83 @@ class GameplayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gameplay, container, false)
+        val fragment = inflater.inflate(R.layout.fragment_gameplay, container, false)
+        setReferances(fragment)
+        game.deck.shuffle()
+        game.addPlayer("Nils", 100)
+        game.players[0].makeBet(10)
+        game.dealInitialCards()
+        updatePlayerCards(game.players[0].hands[0].cards)
+        updatePlayerCardValue(game.getBlackJackValue(game.players[0].hands[0].cards))
+        hitButton.setOnClickListener{
+            onHitPress()
+        }
+
+
+        return fragment
+
+    }
+
+    fun setReferances(fragment: View){
+        playerCard1 = fragment.findViewById(R.id.first_card_player)
+        playerCard2 = fragment.findViewById(R.id.second_card_player)
+        playerCard3 = fragment.findViewById(R.id.third_card_player)
+        playerCard4 = fragment.findViewById(R.id.fourth_card_player)
+        playerCard5 = fragment.findViewById(R.id.fifth_card_player)
+        hitButton = fragment.findViewById(R.id.btn_hit)
+        playerCardValueTextView = fragment.findViewById(R.id.card_value_player)
+
+        playerCardImages.add(playerCard1)
+        playerCardImages.add(playerCard2)
+        playerCardImages.add(playerCard3)
+        playerCardImages.add(playerCard4)
+        playerCardImages.add(playerCard5)
+
+
+    }
+
+    fun onHitPress(){
+        game.players[0].addCard(0, game.deck.drawACard())
+        val cards = game.players[0].hands[0].cards
+        updatePlayerCards(cards)
+        updatePlayerCardValue(game.getBlackJackValue(cards))
+    }
+
+    fun updatePlayerCardValue(value: Int){
+        playerCardValueTextView.text = value.toString()
+    }
+
+
+    fun updatePlayerCards(cards:List<Card>){
+        cards.forEachIndexed{index, card ->
+            val imageId = resources.getIdentifier(getImageId(card), "drawable", requireActivity().packageName)
+            playerCardImages[index].setImageResource(imageId)
+
+        }
+
+    }
+
+    fun getImageId(card: Card): String{
+        val builder = StringBuilder()
+        when (card.suit){
+            "Hearts" -> {
+                builder.append("h")
+            }
+            "Diamonds" -> {
+                builder.append("d")
+            }
+            "Clubs" -> {
+                builder.append("c")
+            }
+            "Spades" -> {
+                builder.append("s")
+            }
+        }
+        if (card.number < 10){
+            builder.append(0)
+        }
+        builder.append(card.number)
+        return builder.toString()
     }
 
     companion object {
