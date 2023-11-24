f793105cedae9d649dc7931c26adeb40026a2af2
KT <thun.kristian@gmail.com>
2023-11-24 12:37:01 +0100

    Resolve mess

--- app/src/main/java/com/example/blackjack21/GameplayActivity.kt
+++ app/src/main/java/com/example/blackjack21/GameplayActivity.kt
@@ -24,7 +24,7 @@ class GameplayActivity : AppCompatActivity(), GameplayFragment.GamePlayListener 
         addPlayer("Nils", 100) // Byt mot info from intent
         players[0].makeBet(10)
         deck.shuffle()
-        dealInitialCards()
+
 
 
         if (savedInstanceState == null) {
@@ -66,15 +66,8 @@ class GameplayActivity : AppCompatActivity(), GameplayFragment.GamePlayListener 
         players.add(BlackJackPlayer(name, money))
     }
 
-    fun dealInitialCards() {
+    private fun dealInitialCards() {
         for (i in 1..2) {
-
-    private fun addPlayer(name: String, money: Int){
-        players.add(BlackJackPlayer(name, money))
-    }
-
-    private fun dealInitialCards(){
-        for (i in 1..2){
 
             players.forEach { player ->
                 player.addCard(0, deck.drawACard())
@@ -86,7 +79,8 @@ class GameplayActivity : AppCompatActivity(), GameplayFragment.GamePlayListener 
 
     fun updateDealerCardImages(cards: List<Card>) {
         cards.forEachIndexed { index, card ->
-            val imageName = if (!isDealerTurn && index == 1) "card_back" else getImageId(card)
+            val imageName =
+                if (!isDealerTurn && index == 1) "card_back" else getImageId(card)
             val imageId = resources.getIdentifier(imageName, "drawable", packageName)
             dealerCardsImageViews[index].setImageResource(imageId)
         }
@@ -132,7 +126,7 @@ class GameplayActivity : AppCompatActivity(), GameplayFragment.GamePlayListener 
         return builder.toString()
     }
 
-    fun getPlayerBets() {   
+    fun getPlayerBets() {
 
         players.forEach { player ->
             player.makeBet(50)
@@ -156,36 +150,42 @@ class GameplayActivity : AppCompatActivity(), GameplayFragment.GamePlayListener 
     }
 
 
-    override fun getBlackJackValue(cards: List<Card>): Int{
+    override fun getBlackJackValue(cards: List<Card>): Int {
         var value = 0
         var numberOfAces = 0
-        cards.forEach{card ->
-            when (card.number){
-                2,3,4,5,6,7,8,9, -> {
+        cards.forEach { card ->
+            when (card.number) {
+                2, 3, 4, 5, 6, 7, 8, 9 -> {
                     value += card.number
                 }
-                10,11,12,13 -> {
+
+                10, 11, 12, 13 -> {
                     value += 10
                 }
+
                 14 -> {
                     value += 11
                     numberOfAces++
                 }
             }
-            while (value > 21 && numberOfAces > 0){
+            while (value > 21 && numberOfAces > 0) {
                 value -= 10
                 numberOfAces--
             }
         }
         return value
     }
-  override fun updatePlayerCards(){
-        val fragment = supportFragmentManager.findFragmentById(R.id.fragment_gameplay_container) as? GameplayFragment
+
+    override fun updatePlayerCards() {
+        val fragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_gameplay_container) as? GameplayFragment
         fragment?.updatePlayerCards(players[0].hands[0].cards)
         fragment?.updatePlayerCardValue(getBlackJackValue(players[0].hands[0].cards))
     }
+
     override fun onHitPress() {
         players[0].hands[0].addCard(deck.drawACard())
         updatePlayerCards()
     }
 }
+
