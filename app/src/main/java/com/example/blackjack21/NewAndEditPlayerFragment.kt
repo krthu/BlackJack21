fde810bc750d83d15ae3a51dbd9bb0afef00d314
KT <thun.kristian@gmail.com>
2023-12-06 10:28:36 +0100

    Removed Fragment boilerplate

--- app/src/main/java/com/example/blackjack21/NewAndEditPlayerFragment.kt
+++ app/src/main/java/com/example/blackjack21/NewAndEditPlayerFragment.kt
@@ -11,20 +11,7 @@ import android.widget.TextView
 
 private const val STARTING_MONEY = 1000.0
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NewAndEditPlayerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NewAndEditPlayerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     lateinit var nameTextEdit: EditText
     lateinit var saveTextView: TextView
@@ -34,8 +21,7 @@ class NewAndEditPlayerFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -118,22 +104,6 @@ class NewAndEditPlayerFragment : Fragment() {
 
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment newPlayerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewAndEditPlayerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
